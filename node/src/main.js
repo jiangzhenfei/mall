22d5c5aa7a29068f25a3babe331056df6cd80b5b
江振飞 <zhenfei_jiang@dnt.com.cn>
2018-07-09 16:03:25 +0800

     图片还原为图片，而不是二进制

--- node/src/main.js
+++ node/src/main.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs')
+//文件上传插件
+var multiparty = require('multiparty'); 
 //增加body-parser中间件
 app.use( bodyParser.urlencoded( { extended: false } ) ); 
 app.use(bodyParser.json()); 
 
-var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+
 
 app.use(express.static('static'));
 
@@ -70,7 +71,7 @@ app.post('/goods/add/:id', function( req, res ) {
         desc:  req.body.desc || '',
         money: req.body.money,
         store: req.body.store,
-        img:   '/file/'+req.body.img || '/images/goods.jpg',//保存文件服务器返回给前端的图片路径
+        img:   req.body.img || '/images/goods.jpg',//保存文件服务器返回给前端的图片路径
         id:    goodsId++
     })
     let json = {
@@ -82,36 +83,34 @@ app.post('/goods/add/:id', function( req, res ) {
 });
 
 //上传文件将文件路径返回前端使得前端保存
-app.post('/upload', upload.single('image_cover'),function( req, res ) {
-    console.log(req.file)
-    let json = {
-        code:200,
-        url:req.file.filename,
-        success:true
-    }
-    let result = JSON.stringify( json )
-    res.send( result )
-});
-
-//文件读取
-app.get('/file/:id', function( req, res ) {
-    let id = req.params.id
-    fs.readFile( `./uploads/${id}` ,'binary',function(err,filedata)  { //异步执行  'binary' 二进制流的文件
-        if  (err)  {
-            let json = {
-                code:404,
-                success:false
-            }
-            let result = JSON.stringify( json )
-            res.send( result )
-            return;
-        }else{
-            res.writeHead('200', {'Content-Type': 'image/jpeg'});    //写http头部信息
-            res.end(filedata,'binary'); 
+app.post('/upload', function( req, res ) {
+    var form = new multiparty.Form();//新建表单
+    //设置编辑
+    form.encoding = 'utf-8';
+    //设置图片存储路径
+    form.uploadDir = "static/images/";
+    form.keepExtensions = true;   //保留后缀
+    form.maxFieldsSize = 2*1024*1024; //内存大小
+    form.maxFilesSize= 5*1024*1024;//文件字节大小限制，超出会报错err
+    //表单解析
+    form.parse(req, function(err,fields,files) {
+        //报错处理
+        if(err){
+            var u={"error" :1,"message":'请上传5M以图片'};
+            res.end(JSON.stringify(u));
+            return false;
         }
+        //获取路径
+        var oldpath=files.image_cover[0]['path'];
+        let json = {
+            code:    200,
+            url:     oldpath.split('/').slice(1).join('/'),//应为不需要static开头，使用了 app.use(express.static('static'));
+            success: true
+        }
+        let result = JSON.stringify( json )
+        res.send( result )
     });
 });
-
 
 
 var server = app.listen(3000, function () {
